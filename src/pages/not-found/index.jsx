546452892723dd0716792164874c6741ab0c4122
Ahmet <ahmetarapaslan@gmail.com>
2024-04-07 14:03:45 +0300

    twitter layout

---
+++ src/pages/not-found/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div className=' bg-red-200 py-5 flex items-center justify-center text-red-800'>NotFound</div>
+  )
+}
