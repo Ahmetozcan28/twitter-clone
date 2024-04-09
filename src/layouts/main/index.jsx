e93932fee236167a0f57ddaca3665aee8c2aa95a
Ahmet <ahmetarapaslan@gmail.com>
2024-04-09 17:20:29 +0300

    featrues: tailwind, sidebar,rightbar, menu and logo

--- src/layouts/main/index.jsx
+++ src/layouts/main/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./sidebar";
+import Rightbar from "./rightbar";
 export default function MainLayout() {
   return (
-    <div>
-        <Outlet/>
+    <div className=" w-[1265px] mx-auto flex">
+      <Sidebar />
+      <main className="flex-1 flex gap-[30px] ">
+        <main className="flex-1 max-w-[600px] border-x border-[#2f3336]">
+          <Outlet />
+        </main>
+        <Rightbar />
+      </main>
     </div>
-  )
+  );
 }
